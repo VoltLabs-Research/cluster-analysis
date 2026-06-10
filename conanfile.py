f26f790d2472269f433b94c10f4b00736c345df6
rodyherrera <contact@rodyherrera.com>
2026-06-10 03:12:48 -0400

    chore: update version to 2.0.0 in conanfile.py and plugin.json; upgrade coretoolkit dependency to 2.0.0

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 
 class ClusterAnalysisConan(ConanFile):
     name = "cluster-analysis"
-    version = "1.0.5"
+    version = "2.0.0"
     package_type = "static-library"
     license = "MIT"
     settings = "os", "arch", "compiler", "build_type"
@@ -12,7 +12,7 @@ class ClusterAnalysisConan(ConanFile):
     requires = (
         "boost/1.88.0",
         "onetbb/2021.12.0",
-        "coretoolkit/1.0.0",
+        "coretoolkit/2.0.0",
         "spdlog/1.14.1",
         "nlohmann_json/3.11.3",
     )
